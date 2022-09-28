dc70817dcb0b701614d3b15ecd82917a20e4444c
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-09-28 14:14:19 +0300

    add count function to find out number of searched elements

--- Program.cs
+++ Program.cs
@@ -17,6 +17,16 @@ void PrintArr(string[] arr)
     Console.WriteLine($"{arr[arr.Length - 1]}]");
 }
 
+int CountString(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) count++;
+    }
+    return count;
+}
+
 Console.WriteLine("*********************************************************************************");
 Console.WriteLine("Задача: Программа должна из имеющегося массива строк сформировать массив из строк,");
 Console.WriteLine("длина которых меньше либо равна 3 символам");

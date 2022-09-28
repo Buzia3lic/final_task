6bc276464d1e7738d43080f00b3e5973ab10b51c
Dmitriy Yurkov <buzia3lic@gmail.com>
2022-09-28 14:14:55 +0300

    add main function for task array

--- Program.cs
+++ Program.cs
@@ -27,6 +27,22 @@ int CountString(string[] arr)
     return count;
 }
 
+string[] TaskArray(string[] arr, int count)
+{
+    string[] result = new string[count];
+    count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            result[count] = arr[i];
+            count++;
+        }
+    }
+    return result;
+}
+
 Console.WriteLine("*********************************************************************************");
 Console.WriteLine("Задача: Программа должна из имеющегося массива строк сформировать массив из строк,");
 Console.WriteLine("длина которых меньше либо равна 3 символам");
